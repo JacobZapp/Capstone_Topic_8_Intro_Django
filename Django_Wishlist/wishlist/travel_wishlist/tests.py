c0ae612419c80aedb88eb3829e4e3423cb34570c
Jacob Zapp <jbzapp01@gmail.com>
2026-03-24 12:44:28 -0500

    unit tests implemented and working

--- Django_Wishlist/wishlist/travel_wishlist/tests.py
+++ Django_Wishlist/wishlist/travel_wishlist/tests.py
@@ -1,3 +1,92 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import Place
 
 # Create your tests here.
+
+class TestHomePage(TestCase):
+
+    def test_home_page_shows_empty_list_for_database(self):
+        home_page_url = reverse('place_list')
+        response = self.client.get(home_page_url) # this will start the server running in background and make a request to the server.
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+        self.assertContains(response, 'You have no places in your wishlist')
+
+
+class TestWishlist(TestCase):
+
+    fixtures = ['test_places']
+
+    def test_wishlist_contains_not_visited_places(self):
+        response = self.client.get(reverse('place_list'))
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+        self.assertContains(response, 'Tokyo')
+        self.assertContains(response, 'New York')
+        self.assertNotContains(response, 'San Francisco')
+        self.assertNotContains(response, 'MOAB')
+
+
+class TestVisitedPage(TestCase):
+
+    def test_displays_not_visited_message_on_visited_page(self):
+        response = self.client.get(reverse('places_visited'))
+        self.assertTemplateUsed(response, 'travel_wishlist/visited.html')
+        self.assertContains(response, 'You have not visited any places yet')
+
+
+class VisitedList(TestCase):
+
+    fixtures = ['test_places']
+
+    def test_visited_list_shows_visited_places(self):
+        response = self.client.get(reverse('places_visited'))
+        self.assertTemplateUsed(response, 'travel_wishlist/visited.html')
+        self.assertContains(response, 'San Francisco')
+        self.assertContains(response, 'Moab')
+        self.assertNotContains(response, 'New York')
+        self.assertNotContains(response, 'Tokyo')
+
+
+class TestAddNewPlace(TestCase):
+
+    def test_add_new_unvisited_place(self):
+        add_place_url = reverse('place_list')
+        new_place_data = {'name': 'China', 'visited': False}
+
+        response = self.client.post(add_place_url, new_place_data, follow=True)
+
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        response_places = response.context['places']
+        self.assertEqual(1, len(response_places)) # check only one place
+        china_from_response = response_places[0]
+
+        china_from_database = Place.objects.get(name='China', visited=False)
+        
+        self.assertEqual(china_from_database, china_from_response)
+
+
+class TestVisitPlace(TestCase):
+
+    fixtures = ['test_places']
+
+    def test_visit_place(self):
+        visit_place_url = reverse('place_was_visited', args=(2,))
+        response = self.client.post(visit_place_url, follow=True)
+
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        self.assertNotContains(response, 'New York')
+        self.assertContains(response, 'Tokyo')
+
+        new_york = Place.objects.get(pk=2)
+        self.assertTrue(new_york.visited)
+
+
+    def test_nonexistent_place(self):
+        visit_nonexistent_place_url = reverse('place_was_visited', args=(123456, ))
+        response = self.client.post(visit_nonexistent_place_url, follow=True)
+        self.assertEqual(404, response.status_code)
+
+    
